338a3408c3bfe2d6555bfece1f11f3424875abec
Kasama <ralegro@bol.com.br>
2017-06-28 15:10:12 -0300

    merged with upstream

--- puzzle_2048.py
+++ puzzle_2048.py
@@ -1,17 +1,18 @@
 from random import randint
 
-class Puzzle2048:
+
+class Puzzle2048(object):
     def __init__(self, size=4, num_starting_squares=2):
         self.game = []
         self.score = 0
-        
+
         for i in range(size):
             self.game += [[0]*size]
-        
+
         for i in range(num_starting_squares):
             self.addNewNumber()
 
-    #Merges values towards the left
+    # Merges values towards the left
     def merge(self):
         for i in range(len(self.game)):
             for j in range(1, len(self.game[0])):
@@ -20,7 +21,7 @@ class Puzzle2048:
                     self.game[i][j] = 0
                     self.score += self.game[i][j-1]
 
-    #Makes the movement to the left 
+    # Makes the movement to the left
     def makeFall(self):
         was_valid = False
         for n in range(len(self.game[0])):
@@ -36,7 +37,7 @@ class Puzzle2048:
         matrix = list(zip(*matrix[::-1]))
         matrix = [list(line) for line in matrix]
         return matrix
-    
+
     def rotateMatrixAntiClockwise(self, matrix):
         matrix = list(reversed(list(zip(*matrix))))
         matrix = [list(line) for line in matrix]
@@ -48,19 +49,19 @@ class Puzzle2048:
         ans = self.makeFall()
         self.game = self.rotateMatrixClockwise(self.game)
         return ans
-    
+
     def down(self):
         self.game = self.rotateMatrixClockwise(self.game)
         self.merge()
         ans = self.makeFall()
         self.game = self.rotateMatrixAntiClockwise(self.game)
         return ans
-    
+
     def left(self):
         self.merge()
         ans = self.makeFall()
         return ans
-    
+
     def right(self):
         self.game = self.rotateMatrixClockwise(self.game)
         self.game = self.rotateMatrixClockwise(self.game)
@@ -76,20 +77,20 @@ class Puzzle2048:
             for j in range(len(self.game[0])):
                 if self.game[i][j] == 0:
                     freeSpots += [(i, j)]
-        
+
         if len(freeSpots) == 0:
             return False
-        
+
         pickedSpot = freeSpots[randint(0, len(freeSpots) - 1)]
-        #Add new number to matrix
-        #With 10% chance of it being a 4
+        # Add new number to matrix
+        # With 10% chance of it being a 4
         if randint(0, 9) == 0:
             self.game[pickedSpot[0]][pickedSpot[1]] = 4
         else:
             self.game[pickedSpot[0]][pickedSpot[1]] = 2
-            
+
         return True
-        
+
     def toArray(self):
         ans = []
         for aux in self.game:
@@ -111,7 +112,7 @@ def doRandomMove(game):
 
 if __name__ == '__main__':
     n = 10000
-    
+
     tot = 0
     curr_max = 0
     for i in range(n):
@@ -120,10 +121,10 @@ if __name__ == '__main__':
         while(game.addNewNumber()):
             doRandomMove(game)
         tot += game.score
-        
+
         max_val = max([max(l) for l in game.game])
         if max_val > curr_max:
             curr_max = max_val
-    
+
     print(tot/n)
     print(curr_max)
