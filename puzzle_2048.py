ffd37c3a7253a7e15f7c0cfde14b74a9ca87576b
Garunts <gasbicalho@gmail.com>
2017-06-27 21:18:48 -0300

    Objectified 2048

--- puzzle_2048.py
+++ puzzle_2048.py
@@ -1,88 +1,86 @@
-import copy
 from random import randint
 
-#Merges values towards the left
-def merge(game):
-	game = copy.deepcopy(game)
-	for i in range(len(game)):
-		for j in range(1, len(game[0])):
-			if game[i][j-1] == game[i][j]:
-				game[i][j-1] *= 2
-				game[i][j] = 0
-	return game
-
-#Makes the movement to the left 
-def makeFall(game):
-	game = copy.deepcopy(game)
-	for n in range(len(game[0])):
-		for i in range(len(game)):
-			for j in range(1, len(game[0])):
-				if game[i][j-1] == 0:
-					game[i][j-1] = game[i][j]
-					game[i][j] = 0
-	return game
-
-def rotateMatrixClockwise(matrix):
-	matrix = list(zip(*matrix[::-1]))
-	matrix = [list(line) for line in matrix]
-	return matrix
-	
-def rotateMatrixAntiClockwise(matrix):
-	matrix = list(reversed(list(zip(*matrix))))
-	matrix = [list(line) for line in matrix]
-	return matrix
-
-def up(game):
-	game = rotateMatrixAntiClockwise(game)
-	game = merge(game)
-	game = makeFall(game)
-	game = rotateMatrixClockwise(game)
-	return game
-	
-def down(game):
-	game = rotateMatrixClockwise(game)
-	game = merge(game)
-	game = makeFall(game)
-	game = rotateMatrixAntiClockwise(game)
-	return game
-	
-def left(game):
-	game = merge(game)
-	game = makeFall(game)
-	return game
-	
-def right(game):
-	game = rotateMatrixClockwise(game)
-	game = rotateMatrixClockwise(game)
-	game = merge(game)
-	game = makeFall(game)
-	game = rotateMatrixClockwise(game)
-	game = rotateMatrixClockwise(game)
-	return game
-
-def addNewNumber(game):
-	game = copy.deepcopy(game)
-	
-	freeSpots = []
-	for i in range(len(game)):
-		for j in range(len(game[0])):
-			if game[i][j] == 0:
-				freeSpots += [(i, j)]
-	
-	if len(freeSpots) == 0:
-		return game, False
-	
-	pickedSpot = freeSpots[randint(0, len(freeSpots) - 1)]
-	#Add new number to matrix
-	#With 10% chance of it being a 4
-	if randint(0, 9) == 0:
-		game[pickedSpot[0]][pickedSpot[1]] = 4
-	else:
-		game[pickedSpot[0]][pickedSpot[1]] = 2
+class Puzzle2048(object):
+	def __init__(self, size=4, num_starting_squares=2):
+		self.game = []
+		self.score = 0
 		
-	return game, True
+		for i in range(size):
+			self.game += [[0]*size]
+		
+		for i in range(num_starting_squares):
+			self.addNewNumber()
 
-def printMatrix(matrix):
-	for line in matrix:
-		print(line)
+	#Merges values towards the left
+	def merge(self):
+		for i in range(len(self.game)):
+			for j in range(1, len(self.game[0])):
+				if self.game[i][j-1] == self.game[i][j]:
+					self.game[i][j-1] *= 2
+					self.game[i][j] = 0
+					self.score += self.game[i][j-1]
+
+	#Makes the movement to the left 
+	def makeFall(self):
+		for n in range(len(self.game[0])):
+			for i in range(len(self.game)):
+				for j in range(1, len(self.game[0])):
+					if self.game[i][j-1] == 0:
+						self.game[i][j-1] = self.game[i][j]
+						self.game[i][j] = 0
+
+	def rotateMatrixClockwise(matrix):
+		matrix = list(zip(*matrix[::-1]))
+		matrix = [list(line) for line in matrix]
+		return matrix
+	
+	def rotateMatrixAntiClockwise(matrix):
+		matrix = list(reversed(list(zip(*matrix))))
+		matrix = [list(line) for line in matrix]
+		return matrix
+
+	def up(self):
+		self.game = rotateMatrixAntiClockwise(self.game)
+		self.merge()
+		self.makeFall()
+		self.game = rotateMatrixClockwise(self.game)
+	
+	def down(self):
+		self.game = rotateMatrixClockwise(self.game)
+		self.merge()
+		self.makeFall()
+		self.game = rotateMatrixAntiClockwise(self.game)
+	
+	def left(self):
+		self.merge()
+		self.makeFall()
+	
+	def right(self):
+		self.game = rotateMatrixClockwise(self.game)
+		self.game = rotateMatrixClockwise(self.game)
+		self.merge()
+		self.makeFall()
+		self.game = rotateMatrixClockwise(self.game)
+		self.game = rotateMatrixClockwise(self.game)
+
+	def addNewNumber(self):
+		freeSpots = []
+		for i in range(len(self.game)):
+			for j in range(len(self.game[0])):
+				if self.game[i][j] == 0:
+					freeSpots += [(i, j)]
+		
+		if len(freeSpots) == 0:
+			return False
+		
+		pickedSpot = freeSpots[randint(0, len(freeSpots) - 1)]
+		#Add new number to matrix
+		#With 10% chance of it being a 4
+		if randint(0, 9) == 0:
+			self.game[pickedSpot[0]][pickedSpot[1]] = 4
+		else:
+			self.game[pickedSpot[0]][pickedSpot[1]] = 2
+			
+		return True
+
 
